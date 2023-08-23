e545ea90169a51532d33b009b5f471924ae8c729
Stone <stone.dummett@gmail.com>
2023-08-23 17:54:16 +0200

    Add some body fields check on POST /api/user endpoint

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,16 +1,48 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import validator from 'validator'
 import prisma from '../../../lib/prisma'
+
 
 // POST /api/user
 // Required fields in body: name, email
 export default async function handle(
-  req: NextApiRequest,
-  res: NextApiResponse,
+    req: NextApiRequest,
+    res: NextApiResponse,
 ) {
-  const result = await prisma.user.create({
-    data: {
-      ...req.body,
-    },
-  })
-  return res.status(201).json(result)
+    if (req.method !== 'POST')
+        return res.status(405).json({ error: 'Method Not Allowed' });
+
+    const { email, name } = req.body;
+
+    // Check for the required body fields
+    const validationError = validateBodyFields(email, name);
+    if (validationError)
+        return res.status(400).json({ error: validationError });
+
+    return post(req, res);
+}
+
+function validateBodyFields(email: unknown, name: unknown) {
+    if (!email)
+        return 'email is required.';
+
+    if (!validator.isEmail(email))
+        return 'Invalid email format.';
+
+    if (!name)
+        return 'name is required.';
+
+    return null;
+}
+
+async function post(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    const result = await prisma.user.create({
+        data: {
+            ...req.body,
+        },
+    })
+    return res.status(201).json(result)
 }
