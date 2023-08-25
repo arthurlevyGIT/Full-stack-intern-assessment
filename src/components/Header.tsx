ff2a145d1c4715527039b963586064ecb221c665
Stone <stone.dummett@gmail.com>
2023-08-25 16:46:40 +0200

    [FRONTEND]: Conditionnally render Header buttons depending on if the user is authenticated or not

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useAuth } from '../context/authContext';
 import styles from './Header.module.css'
 
 export default function Header() {
   const routePathName = usePathname()
   const isActive: (pathname: string) => boolean = (pathname) =>
     routePathName === pathname
+
+  const { authenticated } = useAuth();
 
   return (
     <nav>
@@ -17,21 +20,32 @@ export default function Header() {
             Blog
           </a>
         </Link>
-        <Link href="/drafts" legacyBehavior>
-          <a data-active={isActive('/drafts')}>Drafts</a>
-        </Link>
+        {/* Render Drafts links if authenticated */}
+        {authenticated && (
+          <Link href="/drafts" legacyBehavior>
+            <a data-active={isActive('/drafts')}>Drafts</a>
+          </Link>
+        )}
       </div>
       <div className={styles.right}>
-        <Link href="/signup" legacyBehavior>
-          <a data-active={isActive('/signup')}>Signup</a>
-        </Link>
-        <Link href='signin' legacyBehavior>
-          <a data-active={isActive('/signin')}>Signin</a>
-        </Link>
-        <Link href="/create" legacyBehavior>
-          <a data-active={isActive('/create')}>+ Create draft</a>
-        </Link>
+        {/* Render Signup and Signin links if not authenticated */}
+        {!authenticated && (
+          <>
+            <Link href="/signup" legacyBehavior>
+              <a data-active={isActive('/signup')}>Signup</a>
+            </Link>
+            <Link href="/signin" legacyBehavior>
+              <a data-active={isActive('/signin')}>Signin</a>
+            </Link>
+          </>
+        )}
+        {/* Render Create draft link if authenticated */}
+        {authenticated && (
+          <Link href="/create" legacyBehavior>
+            <a data-active={isActive('/create')}>+ Create draft</a>
+          </Link>
+        )}
       </div>
     </nav>
-  )
+  );
 }
