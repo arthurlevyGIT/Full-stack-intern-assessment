17e8a6e8b5d864f695ea9259013e6588e8488993
Stone <stone.dummett@gmail.com>
2023-08-27 11:23:22 +0200

    Made with ♥ by Stone.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,10 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
-        <body className='bg-gray-900 text-gray-200'>
+        <body className='bg-gray-900 text-gray-200 flex flex-col min-h-screen'>
           <Header />
           <div style={{ padding: "0 2rem" }}>{children}</div>
+        <footer className='text-xs mt-auto'>Made with ♥ by Stone.</footer>
         </body>
       </html>
     </AuthProvider>
